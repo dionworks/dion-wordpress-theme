bd610503f165046aca2855db96d28b37fd5f2a57
tarikcayir <tcayir@dionadworks.com>
2014-12-16 14:15:01 +0200

    Google analytics geolocation added

--- functions.php
+++ functions.php
@@ -279,25 +279,20 @@ function userCustomJs(){
 function googleAnalytics(){
     global $options;
 
-    if ($options["googleAnalytics"]) {
-        ?>
+    if ($options["googleAnalytics"]): ?>
         <script type="text/javascript">
             // Google Analytics
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', '<?php echo $options["googleAnalytics"]; ?>']);
-            _gaq.push(['_trackPageview']);
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            (function () {
-                var ga = document.createElement('script');
-                ga.type = 'text/javascript';
-                ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google_analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(ga, s);
-            })();
+            ga('create', '<?php echo $options["googleAnalytics"]; ?>', 'auto');
+            ga('require', 'displayfeatures');
+            ga('send', 'pageview');
         </script>
     <?php
-    }
+    endif; 
 }
 
 /**
